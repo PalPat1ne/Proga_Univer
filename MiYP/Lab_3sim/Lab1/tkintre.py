ac6481e822c0c1c09d036cc68509d317f352b3ab
PalPat1ne <serg12345290@gmail.com>
2022-09-30 13:39:31 -0400

    Измене типо очистки холста

--- MiYP/Lab_3sim/Lab1/tkintre.py
+++ MiYP/Lab_3sim/Lab1/tkintre.py
@@ -27,7 +27,7 @@ def grafic_1():
     y6_axe.append(yy)
     yy =(470,20)
     y6_axe.append(yy)
-    convas.create_line(y6_axe,fill='black')
+    canvas.create_line(y6_axe,fill='black')
 
 def grafic_2():
     points = []
@@ -36,7 +36,7 @@ def grafic_2():
         y=-1000/x
         pp=(x+40,y+320)
         points.append(pp)
-    convas.create_line(points,fill='red',smooth=1)
+    canvas.create_line(points,fill='red',smooth=1)
 
 def grafic_3():
     points = []
@@ -45,7 +45,7 @@ def grafic_3():
         y=-0.05*x*x
         pp1=(x+23,y+334)
         points.append(pp1)
-    convas.create_line(points,fill='blue',smooth=1)
+    canvas.create_line(points,fill='blue',smooth=1)
 
 def grafic_4():
     points = []
@@ -54,10 +54,53 @@ def grafic_4():
         y=100*math.cos(i)
         pp1=(x+23,y+234)
         points.append(pp1)
-    convas.create_line(points,fill='green',smooth=1)
+    canvas.create_line(points,fill='green',smooth=1)
+    
 def clear():
-    convas.create_rectangle(23,20,470,334,outline="white",fill='white')
+    canvas.delete('all')
+    osi()
+    #canvas.create_rectangle(23,20,470,334,outline="white",fill='white')
 
+def osi():
+    canvas.create_text(10,10,text='Y')
+    canvas.create_text(470,350,text='X')
+    canvas.create_text(10,350,text='0')
+    y_axe=[] 
+    yy = (22,10)
+    y_axe.append(yy)
+    yy =(22,335)
+    y_axe.append(yy)
+    canvas.create_line(y_axe,fill='black')
+    y1_axe=[]
+    yy = (22,335)
+    y1_axe.append(yy)
+    yy =(470,335)
+    y1_axe.append(yy)
+    canvas.create_line(y1_axe,fill='black') 
+    y2_axe=[]
+    yy = (460,330)
+    y2_axe.append(yy)
+    yy =(470,335)
+    y2_axe.append(yy)
+    canvas.create_line(y2_axe,fill='black') 
+    y3_axe=[]
+    yy = (460,340)
+    y3_axe.append(yy)
+    yy =(470,335)
+    y3_axe.append(yy)
+    canvas.create_line(y3_axe,fill='black')
+    y4_axe=[]
+    yy = (22,10)
+    y4_axe.append(yy)
+    yy =(17,20)
+    y4_axe.append(yy)
+    canvas.create_line(y4_axe,fill='black')
+    y5_axe=[]
+    yy = (22,10)
+    y5_axe.append(yy)
+    yy =(27,20)
+    y5_axe.append(yy)
+    canvas.create_line(y5_axe,fill='black')
 
 button = tkinter.Button(
     text="Закрыть ✖",
@@ -96,49 +139,12 @@ menu.config(width=12 , height=1)
 menu.pack(anchor='nw')
 
 
-convas = tkinter.Canvas(tk)
-convas['height']=360
-convas['width']=480
-convas['background']='#ffffff'
-convas['borderwidth']='2'
-convas.pack()
-convas.create_text(10,10,text='Y')
-convas.create_text(470,350,text='X')
-convas.create_text(10,350,text='0')
-y_axe=[]
-yy = (22,10)
-y_axe.append(yy)
-yy =(22,335)
-y_axe.append(yy)
-convas.create_line(y_axe,fill='black')
-y1_axe=[]
-yy = (22,335)
-y1_axe.append(yy)
-yy =(470,335)
-y1_axe.append(yy)
-convas.create_line(y1_axe,fill='black') 
-y2_axe=[]
-yy = (460,330)
-y2_axe.append(yy)
-yy =(470,335)
-y2_axe.append(yy)
-convas.create_line(y2_axe,fill='black') 
-y3_axe=[]
-yy = (460,340)
-y3_axe.append(yy)
-yy =(470,335)
-y3_axe.append(yy)
-convas.create_line(y3_axe,fill='black')
-y4_axe=[]
-yy = (22,10)
-y4_axe.append(yy)
-yy =(17,20)
-y4_axe.append(yy)
-convas.create_line(y4_axe,fill='black')
-y5_axe=[]
-yy = (22,10)
-y5_axe.append(yy)
-yy =(27,20)
-y5_axe.append(yy)
-convas.create_line(y5_axe,fill='black')
+canvas = tkinter.Canvas(tk)
+canvas['height']=360
+canvas['width']=480
+canvas['background']='#ffffff'
+canvas['borderwidth']='2'
+canvas.pack()
+osi()
+
 tk.mainloop()
